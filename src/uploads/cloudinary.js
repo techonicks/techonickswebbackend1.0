19b6c678479508834977bf019c8e4e9de6bd5b9a
SayanSutradhar1 <sayan321x@gmail.com>
2024-12-02 20:19:33 +0530

    test

--- src/uploads/cloudinary.js
+++ src/uploads/cloudinary.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const uploadFile = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const localFilePath = path.join(__dirname, "/content/temp.jpg");
+  const localFilePath = path.join(__dirname, "../../tmp/temp.jpg");
   try {
     if (!localFilePath) return null;
     const response = await cloudinary.uploader.upload(localFilePath, {
