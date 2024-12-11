cc3fc62e2002e6598591d25eb17fb7c0bcf09261
SayanSutradhar1 <sayan321x@gmail.com>
2024-12-11 22:19:35 +0530

    one frontend url added

--- src/app.js
+++ src/app.js
@@ -1,32 +1,36 @@
-import express from 'express';
-import { testRouter } from './routes/test.route.js';
-import { BASE_ROUTE } from './constants.js';
-import { userRouter } from './routes/user.route.js';
-import cookieParser from 'cookie-parser';
-import cors from "cors"
-import { profileRoute } from './routes/profile.route.js';
+import express from "express";
+import { testRouter } from "./routes/test.route.js";
+import { BASE_ROUTE } from "./constants.js";
+import { userRouter } from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { profileRoute } from "./routes/profile.route.js";
 
 const app = express();
 
 app.use(
-    cors({
-      origin: ["http://localhost:3000","https://techonicks.vercel.app"],
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-  );
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://techonicks.vercel.app",
+      "https://techonickswebsite-git-dev-techonicks-projects.vercel.app/",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Redirections
 
 // Routes
 
-app.use("/",testRouter)
-app.use(`${BASE_ROUTE}/user`,userRouter)
-app.use(`${BASE_ROUTE}/user/profile`,profileRoute)
+app.use("/", testRouter);
+app.use(`${BASE_ROUTE}/user`, userRouter);
+app.use(`${BASE_ROUTE}/user/profile`, profileRoute);
 
-export { app }
+export { app };
