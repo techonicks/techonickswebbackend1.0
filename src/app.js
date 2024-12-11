cee38194b3143bcc2144341ef6148b62f0477cb0
SayanSutradhar1 <sayan321x@gmail.com>
2024-12-12 00:07:14 +0530

    minor 6

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin : "https://techonickswebsite-git-dev-techonicks-projects.vercel.app",
+    origin : "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
